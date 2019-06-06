70e4d74ad25476268c0f60596a8a43e803320425
Majorafr <corentin.jean.gourmel@gmail.com>
2019-06-06 18:11:31 +0200

    06-06-2019

--- Angular/OpenClassroom/mon-premier-projet/src/app/appareil/appareil.component.ts
+++ Angular/OpenClassroom/mon-premier-projet/src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appareil.component.scss']
 })
 export class AppareilComponent implements OnInit {
-
+  
+  @Input() appareilName : string;
+  @Input() appareilStatus: string;
+  
+  
   constructor() { }
 
   ngOnInit() {
   }
-
+  getStatus(){
+    return this.appareilStatus;
+  }
 }
